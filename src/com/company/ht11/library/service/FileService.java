078d35418c7a73e60b9946a5be07d449bf5ae46b
volha-bibik <Volha_Bibik@epam.com>
2018-04-26 10:00:47 +0300

    Add files via upload

--- src/com/company/ht11/library/service/FileService.java
+++ src/com/company/ht11/library/service/FileService.java
@@ -17,9 +17,11 @@ public class FileService {
                 line = null;
             }
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("File not found!");
+            fileContext = null;
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("IO error");
+            fileContext = null;
         }
     }
 
@@ -27,15 +29,23 @@ public class FileService {
         String[] parts;
         for (String line1 : fileContext) {
             parts = line1.split(", ");
-            switch(parts[0]) {
-                case "Newspaper": shell.add(new Newspaper(parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3]),Integer.parseInt(parts[4]), Boolean.parseBoolean(parts[5])));
-                    break;
-                case "Magazine": shell.add(new Magazine(parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3]),Integer.parseInt(parts[4]), Boolean.parseBoolean(parts[5])));
-                    break;
-                case "ProgrammingBook": shell.add(new ProgrammingBook(parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3]), parts[4], parts[5], Integer.parseInt(parts[6]), parts[7], parts[8]));
-                    break;
-                case "ScientificBook": shell.add(new ScientificBook(parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3]), parts[4], parts[5], Integer.parseInt(parts[6]), parts[7]));
-                    break;
+            try {
+                switch (parts[0]) {
+                    case "Newspaper":
+                        shell.add(new Newspaper(parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3]), Integer.parseInt(parts[4]), Boolean.parseBoolean(parts[5])));
+                        break;
+                    case "Magazine":
+                        shell.add(new Magazine(parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3]), Integer.parseInt(parts[4]), Boolean.parseBoolean(parts[5])));
+                        break;
+                    case "ProgrammingBook":
+                        shell.add(new ProgrammingBook(parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3]), parts[4], parts[5], Integer.parseInt(parts[6]), parts[7], parts[8]));
+                        break;
+                    case "ScientificBook":
+                        shell.add(new ScientificBook(parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3]), parts[4], parts[5], Integer.parseInt(parts[6]), parts[7]));
+                        break;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Incorrect data provided (some fields are empty), please check it!");
             }
 
         }
